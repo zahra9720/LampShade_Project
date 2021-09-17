fd5cdb332fac4390998bc438110247fa37604fb5
zahra jebeli <zahra.jebeli79@gmail.com>
2021-09-17 22:52:24 +0430

    ProductCategory Management Implemented

---
+++ Lampshade/ShopManagement.Application/ProductCategoryApplication.cs
@@ -0,0 +1,60 @@
+﻿using _0_Framework.Application;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using System.Collections.Generic;
+
+namespace ShopManagement.Application
+{
+    public class ProductCategoryApplication : IProductCategoryApplication
+    {
+        private readonly IProductCategoryRepository _productCategoryRepository;
+        public ProductCategoryApplication(IProductCategoryRepository productCategoryRepository)
+        {
+            _productCategoryRepository = productCategoryRepository;
+        }
+        public OperationResult Create(CreateProductCategory command)
+        {
+            var operation = new OperationResult();
+            if (_productCategoryRepository.Exists(x => x.Name == command.Name))
+                return operation.Faild("امکان ثبت رکورد تکراری وجود ندارد.لطفا مجدد تلاش فرمایید.");
+
+            var slug = command.Slug.Slugyfi();
+            var productCategory = new ProductCategory(command.Name, command.Description,
+                command.Picture, command.PictureAlt, command.PictureTitle, command.Keywords,
+                command.MetaDescription, slug);
+
+            _productCategoryRepository.Create(productCategory);
+            _productCategoryRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+        public OperationResult Edit(EditProductCategory command)
+        {
+            var operation = new OperationResult();
+            var productCategory = _productCategoryRepository.Get(command.Id);
+            if (productCategory == null)
+                return operation.Faild("رکورد با اطلاعات درخواست شده یافت نشد. لطفا مجدد تلاش فرمایید.");
+
+            if (_productCategoryRepository.Exists(x => x.Name == command.Name && x.Id != command.Id))
+                return operation.Faild("امکان ثبت رکورد تکراری وجود ندارد.لطفا مجدد تلاش فرمایید.");
+
+            var slug = command.Slug.Slugyfi();
+            productCategory.Edit(command.Name, command.Description,
+                command.Picture, command.PictureAlt, command.PictureTitle, command.Keywords,
+                command.MetaDescription, slug);
+
+            _productCategoryRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+        public EditProductCategory GetDetails(long id)
+        {
+            return _productCategoryRepository.GetDetails(id);
+        }
+
+        public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            return _productCategoryRepository.Search(searchModel);
+        }
+    }
+}
